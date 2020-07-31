06e39cdb7ff6c496900da4f5201a6ae7a2688dc6
Rick Rainey <rickra@microsoft.com>
2020-07-30 19:09:05 -0500

    Initial commit.

---
+++ Svc1/Controllers/Svc1Controller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Svc1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Svc1 : ControllerBase
+    {
+        [HttpGet]
+        public String Get()
+        {
+            return "Hello from 'Svc1'";
+        }
+    }
+}
---
+++ Svc2/Controllers/Svc2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Svc2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Svc2 : ControllerBase
+    {
+        [HttpGet]
+        public String Get()
+        {
+            return "Hello from 'Svc2'";
+        }
+    }
+}
